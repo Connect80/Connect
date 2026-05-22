ddaf6d209f36a7a871e9493c721e353ad0e1d88f
Connect80 <nitinmassey88@gmail.com>
2026-05-22 20:51:26 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,26 +1,23 @@
 const express = require('express');
 const app = express();
+const twilio = require('twilio');
 
-// यह लाइन बहुत जरूरी है ताकि सर्वर आपके डेटा को पढ़ सके
 app.use(express.json());
 
-// Twilio की जानकारी
-const accountSid = 'ACe295bf1d213e91330c0921d79067f009'; 
-const authToken = '9499831323a3bf98f82255e8dd3fddb4'; 
-const client = require('twilio')(accountSid, authToken);
+const accountSid = 'ACe295bf1d213e91330c0921d79067f009';
+const authToken = '9499831323a3bf98f82255e8dd3fddb4';
+const client = new twilio(accountSid, authToken);
 
 app.post('/send-otp', (req, res) => {
     const { phoneNumber, otp } = req.body;
-
+    
     client.messages.create({
         body: `आपका OTP कोड है: ${otp}`,
         from: '+18787325922',
         to: phoneNumber
     })
-    .then(message => res.json({ success: true, sid: message.sid }))
+    .then(message => res.json({ success: true, message: "Sent!" }))
     .catch(err => res.status(500).json({ success: false, error: err.message }));
 });
 
-app.listen(3000, () => {
-    console.log('सर्वर 3000 पोर्ट पर चालू है');
-});
+app.listen(3000, () => console.log('Server is running'));

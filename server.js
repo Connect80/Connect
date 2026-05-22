7cb303aad39bbf741a9a90ca087150ae82c4dfd1
Connect80 <nitinmassey88@gmail.com>
2026-05-22 19:29:28 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -1,16 +1,20 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
 
-const accountSid = 'ACe295bf1d213e913'; 
-const authToken = '9499831323a3bf98f8';   
+// यह लाइन बहुत जरूरी है ताकि सर्वर आपके डेटा को पढ़ सके
+app.use(express.json()); 
+
+const accountSid = 'Ace295bf1d213e913';
+const authToken = '9499831323a3bf98f8';
 const client = require('twilio')(accountSid, authToken);
 
 app.post('/send-otp', (req, res) => {
+    // यहाँ से कोड डेटा उठाएगा
     const { phoneNumber, otp } = req.body;
+    
     client.messages.create({
         body: `आपका OTP कोड है: ${otp}`,
-        from: '+18787325922', 
+        from: '+18787325922',
         to: phoneNumber
     })
     .then(message => res.json({ success: true, sid: message.sid }))

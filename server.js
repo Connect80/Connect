c5fcdcf52ef4c57657a36edb7af61e6bec6ba7b8
Connect80 <nitinmassey88@gmail.com>
2026-05-23 17:37:39 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -5,19 +5,36 @@ const twilio = require('twilio');
 app.use(express.json());
 
 const accountSid = 'ACe295bf1d213e91330c0921d79067f009';
-const authToken = '9499831323a3bf98f82255e8dd3fddb4';
+const authToken = 'YOUR_AUTH_TOKEN';
+const serviceSid = 'VAdca74f35326707c404bd5214dbbfd84e';
+const twilioPhoneNumber = '+18787325922';
+
 const client = new twilio(accountSid, authToken);
 
 app.post('/send-otp', (req, res) => {
-    const { phoneNumber, otp } = req.body;
-    
-    client.messages.create({
-        body: `आपका OTP कोड है: ${otp}`,
-        from: '+18787325922',
-        to: phoneNumber
-    })
-    .then(message => res.json({ success: true, message: "Sent!" }))
-    .catch(err => res.status(500).json({ success: false, error: err.message }));
+    const phoneNumber = '+918690347348';
+
+    client.verify.v2.services(serviceSid)
+        .verifications
+        .create({ 
+            to: phoneNumber, 
+            channel: 'sms' 
+        })
+        .then(verification => {
+            res.status(200).json({ 
+                success: true, 
+                sid: verification.sid,
+                sender: twilioPhoneNumber 
+            });
+        })
+        .catch(err => {
+            res.status(500).json({ 
+                success: false, 
+                error: err.message 
+            });
+        });
 });
 
-app.listen(3000, () => console.log('Server is running'));
+app.listen(3000, () => {
+    console.log('Server is running on port 3000');
+});
